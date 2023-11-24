86a550874fecbbd4071dc04ac92b7d49982e3aee
Cristóvão Zuppardo Rufino <cristovaozr@gmail.com>
2023-11-24 20:12:58 -0300

    Blocos para o projeto final

---
+++ ProjetoFinal/projeto_final.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Deep Space Modulation Simulator
+# Author: Cristóvão, José Ribamar e Robson
+# GNU Radio version: 3.10.8.0
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+import os
+import sys
+sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from rs_ber_generator2 import rs_ber_generator2  # grc-generated hier_block
+import sip
+import numpy
+
+
+
+class projeto_final(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Deep Space Modulation Simulator", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Deep Space Modulation Simulator")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "projeto_final")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 3200000
+        self.num_curves = num_curves = 1
+        self.esno = esno = numpy.arange(0, 4, 0.5)
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.rs_ber_generator2_0 = rs_ber_generator2()
+        self.qtgui_bercurve_sink_0 = qtgui.ber_sink_b(
+            esno, #range of esnos
+            1, #number of curves
+            50, #ensure at least
+            (-7.0), #cutoff
+            [], #indiv. curve names
+            None # parent
+        )
+        self.qtgui_bercurve_sink_0.set_update_time(0.10)
+        self.qtgui_bercurve_sink_0.set_y_axis((-10), 0)
+        self.qtgui_bercurve_sink_0.set_x_axis(esno[0], esno[-1])
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_bercurve_sink_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_bercurve_sink_0.set_line_label(i, labels[i])
+            self.qtgui_bercurve_sink_0.set_line_width(i, widths[i])
+            self.qtgui_bercurve_sink_0.set_line_color(i, colors[i])
+            self.qtgui_bercurve_sink_0.set_line_style(i, styles[i])
+            self.qtgui_bercurve_sink_0.set_line_marker(i, markers[i])
+            self.qtgui_bercurve_sink_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_bercurve_sink_0_win = sip.wrapinstance(self.qtgui_bercurve_sink_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_bercurve_sink_0_win)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.analog_random_uniform_source_x_0 = analog.random_uniform_source_b(0, 2, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_random_uniform_source_x_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.rs_ber_generator2_0, 1), (self.qtgui_bercurve_sink_0, 1))
+        self.connect((self.rs_ber_generator2_0, 12), (self.qtgui_bercurve_sink_0, 12))
+        self.connect((self.rs_ber_generator2_0, 13), (self.qtgui_bercurve_sink_0, 13))
+        self.connect((self.rs_ber_generator2_0, 4), (self.qtgui_bercurve_sink_0, 4))
+        self.connect((self.rs_ber_generator2_0, 5), (self.qtgui_bercurve_sink_0, 5))
+        self.connect((self.rs_ber_generator2_0, 11), (self.qtgui_bercurve_sink_0, 11))
+        self.connect((self.rs_ber_generator2_0, 10), (self.qtgui_bercurve_sink_0, 10))
+        self.connect((self.rs_ber_generator2_0, 14), (self.qtgui_bercurve_sink_0, 14))
+        self.connect((self.rs_ber_generator2_0, 8), (self.qtgui_bercurve_sink_0, 8))
+        self.connect((self.rs_ber_generator2_0, 2), (self.qtgui_bercurve_sink_0, 2))
+        self.connect((self.rs_ber_generator2_0, 9), (self.qtgui_bercurve_sink_0, 9))
+        self.connect((self.rs_ber_generator2_0, 0), (self.qtgui_bercurve_sink_0, 0))
+        self.connect((self.rs_ber_generator2_0, 7), (self.qtgui_bercurve_sink_0, 7))
+        self.connect((self.rs_ber_generator2_0, 15), (self.qtgui_bercurve_sink_0, 15))
+        self.connect((self.rs_ber_generator2_0, 6), (self.qtgui_bercurve_sink_0, 6))
+        self.connect((self.rs_ber_generator2_0, 3), (self.qtgui_bercurve_sink_0, 3))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "projeto_final")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+
+    def get_num_curves(self):
+        return self.num_curves
+
+    def set_num_curves(self, num_curves):
+        self.num_curves = num_curves
+
+    def get_esno(self):
+        return self.esno
+
+    def set_esno(self, esno):
+        self.esno = esno
+
+
+
+
+def main(top_block_cls=projeto_final, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
---
+++ ProjetoFinal/test_bpsk.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: cristovaozr
+# GNU Radio version: 3.10.8.0
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import dtv
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import sip
+
+
+
+class test_bpsk(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "test_bpsk")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.qtgui_const_sink_x_0_0 = qtgui.const_sink_c(
+            1024, #size
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_const_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_const_sink_x_0_0.set_y_axis((-2), 2)
+        self.qtgui_const_sink_x_0_0.set_x_axis((-2), 2)
+        self.qtgui_const_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0_0.enable_grid(False)
+        self.qtgui_const_sink_x_0_0.enable_axis_labels(True)
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        styles = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_const_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_const_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_const_sink_x_0_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_const_sink_x_0_0_win)
+        self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
+            1024, #size
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_const_sink_x_0.set_update_time(0.10)
+        self.qtgui_const_sink_x_0.set_y_axis((-2), 2)
+        self.qtgui_const_sink_x_0.set_x_axis((-2), 2)
+        self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0.enable_grid(False)
+        self.qtgui_const_sink_x_0.enable_axis_labels(True)
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        styles = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_const_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
+        self.dtv_dvbt_reed_solomon_enc_0_0 = dtv.dvbt_reed_solomon_enc(2, 8, 0x11d, 255, 223, 16, 0, 1)
+        self.dtv_dvbt_reed_solomon_enc_0 = dtv.dvbt_reed_solomon_enc(2, 8, 0x11d, 255, 223, 16, 0, 1)
+        self.blocks_vector_to_stream_0_0 = blocks.vector_to_stream(gr.sizeof_char*1, 255)
+        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_char*1, 255)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_stream_to_vector_0_1 = blocks.stream_to_vector(gr.sizeof_char*1, 223)
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_char*1, 223)
+        self.blocks_null_source_0 = blocks.null_source(gr.sizeof_gr_complex*1)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
+        self.blocks_interleaved_char_to_complex_0 = blocks.interleaved_char_to_complex(False,1.0)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_char_to_float_0 = blocks.char_to_float(1, .5)
+        self.blocks_add_xx_0_0 = blocks.add_vcc(1)
+        self.blocks_add_xx_0 = blocks.add_vcc(1)
+        self.blocks_add_const_vxx_0 = blocks.add_const_ff((-1))
+        self.analog_random_uniform_source_x_0 = analog.random_uniform_source_b(0, 2, 0)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, .1, 0)
+        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_const_source_x_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0_0, 1))
+        self.connect((self.analog_random_uniform_source_x_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.analog_random_uniform_source_x_0, 0), (self.blocks_stream_to_vector_0_1, 0))
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_const_sink_x_0, 0))
+        self.connect((self.blocks_add_xx_0_0, 0), (self.qtgui_const_sink_x_0_0, 0))
+        self.connect((self.blocks_char_to_float_0, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.blocks_interleaved_char_to_complex_0, 0), (self.blocks_add_xx_0_0, 0))
+        self.connect((self.blocks_null_source_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.blocks_stream_to_vector_0, 0), (self.dtv_dvbt_reed_solomon_enc_0, 0))
+        self.connect((self.blocks_stream_to_vector_0_1, 0), (self.dtv_dvbt_reed_solomon_enc_0_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_null_sink_0, 0))
+        self.connect((self.blocks_vector_to_stream_0, 0), (self.blocks_char_to_float_0, 0))
+        self.connect((self.blocks_vector_to_stream_0_0, 0), (self.blocks_interleaved_char_to_complex_0, 0))
+        self.connect((self.dtv_dvbt_reed_solomon_enc_0, 0), (self.blocks_vector_to_stream_0, 0))
+        self.connect((self.dtv_dvbt_reed_solomon_enc_0_0, 0), (self.blocks_vector_to_stream_0_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "test_bpsk")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+
+
+
+
+def main(top_block_cls=test_bpsk, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
