e4eba3361a12176b4099f9ffa8defc725034d7c7
Cristóvão Zuppardo Rufino <cristovaozr@gmail.com>
2023-11-25 10:00:40 -0300

    Adição de canal RAGB, modulador e demodulador BPKS

--- ProjetoFinal/test_bpsk.py
+++ ProjetoFinal/test_bpsk.py
@@ -24,6 +24,7 @@ from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 import sip
+import test_bpsk_epy_block_0 as epy_block_0  # embedded python block
 
 
 
@@ -150,6 +151,7 @@ class test_bpsk(gr.top_block, Qt.QWidget):
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
+        self.epy_block_0 = epy_block_0.BPSK_Encoder_Block(example_param=1.0)
         self.dtv_dvbt_reed_solomon_enc_0_0 = dtv.dvbt_reed_solomon_enc(2, 8, 0x11d, 255, 223, 16, 0, 1)
         self.dtv_dvbt_reed_solomon_enc_0 = dtv.dvbt_reed_solomon_enc(2, 8, 0x11d, 255, 223, 16, 0, 1)
         self.blocks_vector_to_stream_0_0 = blocks.vector_to_stream(gr.sizeof_char*1, 255)
@@ -160,38 +162,31 @@ class test_bpsk(gr.top_block, Qt.QWidget):
         self.blocks_null_source_0 = blocks.null_source(gr.sizeof_gr_complex*1)
         self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_gr_complex*1)
         self.blocks_interleaved_char_to_complex_0 = blocks.interleaved_char_to_complex(False,1.0)
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
-        self.blocks_char_to_float_0 = blocks.char_to_float(1, .5)
         self.blocks_add_xx_0_0 = blocks.add_vcc(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
-        self.blocks_add_const_vxx_0 = blocks.add_const_ff((-1))
         self.analog_random_uniform_source_x_0 = analog.random_uniform_source_b(0, 2, 0)
         self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, .1, 0)
-        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_const_source_x_0, 0), (self.blocks_float_to_complex_0, 1))
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0_0, 1))
         self.connect((self.analog_random_uniform_source_x_0, 0), (self.blocks_stream_to_vector_0, 0))
         self.connect((self.analog_random_uniform_source_x_0, 0), (self.blocks_stream_to_vector_0_1, 0))
-        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_float_to_complex_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.blocks_add_xx_0_0, 0), (self.qtgui_const_sink_x_0_0, 0))
-        self.connect((self.blocks_char_to_float_0, 0), (self.blocks_add_const_vxx_0, 0))
-        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.blocks_interleaved_char_to_complex_0, 0), (self.blocks_add_xx_0_0, 0))
         self.connect((self.blocks_null_source_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_stream_to_vector_0, 0), (self.dtv_dvbt_reed_solomon_enc_0, 0))
         self.connect((self.blocks_stream_to_vector_0_1, 0), (self.dtv_dvbt_reed_solomon_enc_0_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.blocks_null_sink_0, 0))
-        self.connect((self.blocks_vector_to_stream_0, 0), (self.blocks_char_to_float_0, 0))
+        self.connect((self.blocks_vector_to_stream_0, 0), (self.epy_block_0, 0))
         self.connect((self.blocks_vector_to_stream_0_0, 0), (self.blocks_interleaved_char_to_complex_0, 0))
         self.connect((self.dtv_dvbt_reed_solomon_enc_0, 0), (self.blocks_vector_to_stream_0, 0))
         self.connect((self.dtv_dvbt_reed_solomon_enc_0_0, 0), (self.blocks_vector_to_stream_0_0, 0))
+        self.connect((self.epy_block_0, 0), (self.blocks_add_xx_0, 0))
 
 
     def closeEvent(self, event):
---
+++ ProjetoFinal/test_bpsk_epy_block_0.py
@@ -0,0 +1,37 @@
+"""
+Embedded Python Blocks:
+
+Each time this file is saved, GRC will instantiate the first class it finds
+to get ports and parameters of your block. The arguments to __init__  will
+be the parameters. All of them are required to have default values!
+"""
+
+import numpy as np
+from gnuradio import gr
+
+
+class BPSK_Encoder_Block(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
+    """Embedded Python Block example - a simple multiply const"""
+
+    def __init__(self, example_param=1.0):  # only default arguments here
+        """arguments to this function show up as parameters in GRC"""
+        gr.sync_block.__init__(
+            self,
+            name='Embedded Python Block',   # will show up in GRC
+            in_sig=[np.byte],
+            out_sig=[np.complex64]
+        )
+        # if an attribute with the same name as a parameter is found,
+        # a callback is registered (properties work, too).
+        self.example_param = example_param
+
+    def work(self, input_items, output_items):
+        """example: multiply with constant"""
+        
+        for i, item in enumerate(input_items):
+            if item == 0:
+                output_items[0][i] = -1.0
+            else:
+                output_items[0][i] = 1.0
+
+        return len(output_items[0])
